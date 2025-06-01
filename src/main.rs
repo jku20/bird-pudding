5874e46ac09c9fe62fe2fbd6bbb50865a33c4e10
Ethan Uppal <113849268+ethanuppal@users.noreply.github.com>
2025-05-31 19:44:47 -0700

    feat: Template
    Signed-off-by: Ethan Uppal <113849268+ethanuppal@users.noreply.github.com>

---
+++ src/main.rs
@@ -0,0 +1,25 @@
+use bevy::prelude::*;
+use bevy_ecs_tiled::prelude::*;
+
+fn main() {
+    App::new()
+        // Add Bevy default plugins
+        .add_plugins(DefaultPlugins)
+        // Add bevy_ecs_tiled plugin: note that bevy_ecs_tilemap::TilemapPlugin
+        // will be automatically added as well if it's not already done
+        .add_plugins(TiledMapPlugin::default())
+        // Add our startup function to the schedule and run the app
+        .add_systems(Startup, startup)
+        .run();
+}
+
+fn startup(mut commands: Commands, asset_server: Res<AssetServer>) {
+    // Spawn a Bevy 2D camera
+    commands.spawn(Camera2d);
+
+    // Load a map asset and retrieve the corresponding handle
+    let map_handle: Handle<TiledMap> = asset_server.load("test_level.tmx");
+
+    // Spawn a new entity with this handle
+    commands.spawn(TiledMapHandle(map_handle));
+}
