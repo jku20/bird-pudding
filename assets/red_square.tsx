2c1f829e538e3c19988ec94f9982c1445ac86c34
Ethan Uppal <113849268+ethanuppal@users.noreply.github.com>
2025-05-31 20:53:19 -0700

    feat: Gravity works but is broken
    Signed-off-by: Ethan Uppal <113849268+ethanuppal@users.noreply.github.com>

--- assets/red_square.tsx
+++ assets/red_square.tsx
@@ -1,4 +1,9 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.2" name="red_square" tilewidth="32" tileheight="32" tilecount="1" columns="1">
- <image source="red_square.png" width="32" height="32"/>
-</tileset>
+<? xml version = "1.0" encoding = "UTF-8" ?>
+ <tileset version="1.10" tiledversion="1.11.2" name="red_square" tilewidth="32" tileheight="32" tilecount="1" columns="1">
+  <image source="red_square.png" width="32" height="32" />
+  <tile id="0">
+   <objectgroup draworder="index" id="6">
+    <object id="5" x="0" y="0" width="32" height="32" />
+   </objectgroup>
+  </tile>
+ </tileset>
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use avian2d::prelude::*;
+use avian2d::{math::Vector, prelude::*};
 use bevy::prelude::*;
 use bevy_ecs_tiled::prelude::*;
 use bevy_ecs_tilemap::prelude::*;
@@ -13,6 +13,12 @@ fn main() {
         .add_plugins(TiledPhysicsPlugin::<TiledPhysicsAvianBackend>::default())
         // Load Avian main plugin
         .add_plugins(PhysicsPlugins::default().with_length_unit(100.0))
+        .add_plugins((
+            PhysicsDebugPlugin::default(),
+            PhysicsDiagnosticsPlugin,
+            PhysicsDiagnosticsUiPlugin,
+        ))
+        .insert_resource(Gravity(Vector::NEG_Y * 100.0))
         // Add our startup function to the schedule and run the app
         .add_systems(Startup, startup)
         .run();
@@ -26,5 +32,23 @@ fn startup(mut commands: Commands, asset_server: Res<AssetServer>) {
     let map_handle: Handle<TiledMap> = asset_server.load("test_level.tmx");
 
     // Spawn a new entity with this handle
-    commands.spawn((TiledMapHandle(map_handle), TilemapAnchor::Center));
+    commands.spawn((
+        TiledMapHandle(map_handle),
+        TilemapAnchor::Center,
+        TiledPhysicsSettings::<TiledPhysicsAvianBackend>::default(),
+    ));
+
+    commands.spawn((
+        Sprite {
+            color: Color::srgb(0.0, 0.0, 1.0),
+            custom_size: Some(Vec2::new(32.0, 32.0)),
+            ..default()
+        },
+        // Transform::from_xyz(0.0, 100.0, 0.0),
+        // GlobalTransform::default(),
+        RigidBody::Dynamic,
+        // LockedAxes::ROTATION_LOCKED,
+        // Restitution::ZERO.with_combine_rule(CoefficientCombine::Min),
+        Collider::rectangle(32.0, 32.0),
+    ));
 }
